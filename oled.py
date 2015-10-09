881dfd1c033ed49768b163da5c83a45c939dde3b
Trueffelwurm <jens@trueffelwurm.de>
2015-10-09 08:26:04 +0200

    Changed color mode from plalette to RGB, added Image function

--- oled.py
+++ oled.py
@@ -1,8 +1,10 @@
 import smbus
+import oled_image2
 import time
 
 BUS_ID = 1
 BUS_ADDR = 0x27
+imgSent=False
 
 def dayName(dow):
         days = {
@@ -31,6 +33,8 @@ def sendValue(bus,val):
    bus.write_byte(BUS_ADDR, val)
 
 def clear(bus):
+   global imgSent
+   imgSent=False
    sendWord(bus,"CL")
 
 def writeText(bus, text):
@@ -47,35 +51,26 @@ def setFont(bus,font):
    sendWord(bus,"SF")
    sendValue(bus,font)
 
-def setColor(bus,color):
-   sendWord(bus,"SC")
-   sendValue(bus,color)
+def setColor(bus,r,g,b):
+   sendWord(bus,"ESC")
+   sendValue(bus,r)
+   sendValue(bus,g)
+   sendValue(bus,b)
 
 def grey(bus,bright):
-        if(bright==0):
-                setColor(bus,0)
-        else:
-                if(bright==1):
-                        setColor(bus,182)
-                else:
-                        setColor(bus,218)
+   setColor(bus,(bright*100+15),(bright*100+15),(bright*100+15))
 
 def blue(bus,bright):
-   setColor(bus,bright+5)
+   setColor(bus,0,0,(bright*120+15))
 
 def green(bus,bright):
-   if(bright==0):
-        bright=1
-   bright=((bright*12)+8)
-   if(bright>28):
-      bright=28
-   setColor(bus,bright)
+   setColor(bus,0,(bright*118+19),0)
 
 def red(bus,bright):
-   bright=((bright*64)+64)
-   if(bright>224):
-      bright=224
-   setColor(bus,bright)
+   setColor(bus,(bright*120+15),0,0)
+
+def black(bus):
+   setColor(bus,0,0,0)
 
 def setPos(bus,x,y):
    sendWord(bus,"TP")
@@ -88,10 +83,9 @@ def screenTimes(bus,next):
         else:
                 bright=0
         if(next):
-                print next
-                row=0
+		clear(bus)
+                row=1
                 for timer in next:
-                        print timer
                         setFont(bus,0)
                         if(timer[3]):
                                 green(bus,bright)
@@ -104,3 +98,28 @@ def screenTimes(bus,next):
         blue(bus,bright)
         setPos(bus,13,3)
         writeText(bus,time.strftime("%H:%M", time.localtime()))
+
+def screenRunning(bus):
+	global imgSent
+	sendWord(bus,"DM^")
+	setFont(bus,120)
+	black(bus)
+	if(imgSent):
+		setPos(bus,13,3)
+		#writeText(bus,time.strftime("%H:%M", (int(time.localtime())-60)))
+	else:
+		imgSent=True
+		drawImage(bus,0,0,160,128,oled_image2.sun())
+	setPos(bus,13,3)
+
+def drawImage(bus,x,y,w,h,data):
+	#sendWord(bus,"SSS")
+	#print((len(data)+9))
+	#sendValue(bus,(len(data)+9))
+	sendWord(bus,"EDIM1")
+	sendValue(bus,x)
+	sendValue(bus,y)
+	sendValue(bus,w)
+	sendValue(bus,h)
+	for b in data:
+		sendValue(bus,b)
