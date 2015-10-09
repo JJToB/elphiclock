918b2ef0547da6691fe9aff3e8b117eb63e1d366
Trueffelwurm <jens@trueffelwurm.de>
2015-10-09 08:27:01 +0200

    Some major changes in timing

--- stripe.py
+++ stripe.py
@@ -3,7 +3,7 @@ import time
 import neopixel
 
 # LED strip configuration:
-LED_COUNT   = 60      # Number of LED pixels.
+LED_COUNT   = 6      # Number of LED pixels.
 LED_PIN     = 18      # GPIO pin connected to the pixels (must support PWM!).
 LED_FREQ_HZ = 800000  # LED signal frequency in hertz (usually 800khz)
 LED_DMA     = 5       # DMA channel to use for generating signal (try 5)
@@ -11,6 +11,7 @@ LED_INVERT  = False   # True to invert the signal (when using NPN transistor lev
 
 global mode, mode_step, modes, mode_position, strip, fb
 mode=0;
+mode_delay=0;
 mode_step=1
 mode_position=0
 
@@ -42,6 +43,16 @@ def refresh_LEDs():
 		mode_mood()
         if(mode>=0):
                 drawFB(strip,fb)                                                                    # Send FB to strip
+
+def running():
+        global mode, fb, strip
+	print("Current mode: "+str(mode))
+	if(mode>=2):
+		print("Running")
+		return(True)
+	else:
+		print("Not Running")
+		return(False)
 
 # Color functions
 
@@ -83,34 +94,42 @@ def fadeAll(level,c,direction=0):                                               
 # Modes
 
 def mode_sunrise():
-        global strip, mode, fb, mode_step, mode_position
-        if(mode_step == 1):
-                if(fadeFromMiddle(5,2)):                                                            # Make light blue backlight
-                        mode_step=(mode_step+1)                                                     # Continue with next step
-                        mode_position=0                                                             # Reset position for next step
-        elif(mode_step == 2):
-                fadeFromMiddle(20,0,1)                                                              # Turn to red
-                if(mode_position > ((strip.numPixels()/12)*20)):                                    # When at 1/6 of the half stripe
-                       fadeFromMiddle(40,0,1)                                                       # Start second wave to red
-                       fadeFromMiddle(50,1,1)                                                       # Start fading to yellow
-                       if(mode_position > ((strip.numPixels()/12)*40)):                             # When at 1/3 of the half stripe
-                              if(fadeFromMiddle(60,0,1) and                                         # Start third wave to red
-                                 fadeFromMiddle(60,1,1) ):                                          # Start second wave to yellow
-                                     mode_step=(mode_step+1)                                        # Go to next step
-                mode_position=(mode_position+1)
-        elif(mode_step == 3):
-                if( fadeFromMiddle(80,0) and                                                        # Fade all to white
-                    fadeFromMiddle(80,1) and                                                        # Fade all to white
-                    fadeFromMiddle(80,2) ):                                                         # Fade all to white
-                    mode_step=(mode_step+1)                                                         # Go to next step
-        elif(mode_step == 4):
-                if( fadeFromMiddle(255,0) and                                                       # Fade all to bright white
-                    fadeFromMiddle(255,1) and                                                       # Fade all to bright white
-                    fadeFromMiddle(255,2) ):                                                        # Fade all to bright white
-                    mode_step=(mode_step+1)                                                         # Go to next step
-        else:
-                mode=0                                                                              # Set mode to 0
-                mode_step=1                                                                         # Reset step
+        global strip, mode, fb, mode_step, mode_position, mode_delay
+	if(mode_delay>10):
+		mode_delay=0
+        	if(mode_step == 1):
+                	if(fadeFromMiddle(5,2)):                                                            # Make light blue backlight
+                        	mode_step=(mode_step+1)                                                     # Continue with next step
+                        	mode_position=0                                                             # Reset position for next step
+                        	print("Mode Step: "+str(mode_step))
+        	elif(mode_step == 2):
+                	fadeFromMiddle(20,0,1)                                                              # Turn to red
+                	if(mode_position > ((strip.numPixels()/12)*20)):                                    # When at 1/6 of the half stripe
+                       		fadeFromMiddle(40,0,1)                                                       # Start second wave to red
+                       		fadeFromMiddle(50,1,1)                                                       # Start fading to yellow
+                       		if(mode_position > ((strip.numPixels()/12)*40)):                             # When at 1/3 of the half stripe
+                              		if(fadeFromMiddle(60,0,1) and                                         # Start third wave to red
+                                 		fadeFromMiddle(60,1,1) ):                                          # Start second wave to yellow
+                                     		mode_step=(mode_step+1)                                        # Go to next step
+                                     		print("Mode Step: "+str(mode_step))
+                	mode_position=(mode_position+1)
+        	elif(mode_step == 3):
+                	if( fadeFromMiddle(80,0) and                                                        # Fade all to white
+                    	fadeFromMiddle(80,1) and                                                        # Fade all to white
+                    	fadeFromMiddle(80,2) ):                                                         # Fade all to white
+                    		mode_step=(mode_step+1)                                                         # Go to next step
+                    		print("Mode Step: "+str(mode_step))
+	                  	 
+        	elif(mode_step == 4):
+                	if( fadeFromMiddle(255,0) and                                                       # Fade all to bright white
+                    	fadeFromMiddle(255,1) and                                                       # Fade all to bright white
+                    	fadeFromMiddle(255,2) ):                                                        # Fade all to bright white
+                    		mode_step=(mode_step+1)                                                         # Go to next step
+                    		print("Mode Step: "+str(mode_step))
+        	else:
+			print("Done")
+	else:
+		mode_delay=(mode_delay+1)
 
 def mode_off():
         global mode
@@ -122,9 +141,9 @@ def mode_off():
 def mode_mood():
         global mode
         if(fadeAll(60,0) and                                                                        # Turn all LEDs to mood-color
-           fadeAll(0,1) and
-           fadeAll(60,2)):
-                mode=0                                                                              # Set mode to 0
+           fadeAll(0,1)):
+           fadeAll(60,2)
+#                mode=0                                                                              # Set mode to 0
 
 def mode_none():
         return(False)                                                                               # No need to update stripe
